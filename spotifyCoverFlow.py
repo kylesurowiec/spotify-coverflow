4a8cd79bbcafefbbe45eec694054ff9995697ab9
Kyle <kylesurowiec@me.com>
2017-07-15 22:08:35 -0500

    fully functioning

--- spotifyCoverFlow.py
+++ spotifyCoverFlow.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import pygame
 import io
 import spotipy
@@ -29,26 +30,35 @@ def draw_image(width, height, image_url):
     scale = 1.2
     screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
     screen.fill(black)
+    pygame.mouse.set_visible(False)
     image_str = urlopen(image_url).read()
     image_file = io.BytesIO(image_str)
     image = pygame.image.load(image_file).convert()
     image_resize = pygame.transform.scale(image, (int(image_width * scale), int(image_width * scale)))
     screen.blit(image_resize, (width / 2 - (image_width * scale / 2), height / 2 - (image_height * scale / 2)))
     pygame.display.flip()
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                raise SystemExit
+
             
 ### if spotify oauth token and scope are valid, gets current playing song.
 ### returns JSON, search json for album art link. call draw_image() to display it.
 ### Will need to be threaded or re-timed to update after a song changes
 if token:
     sp = spotipy.Spotify(auth=token)
-    results = sp.currently_playing();
+    results = sp.currently_playing()
     image_src = results["item"]["album"]["images"][0]["url"]
     draw_image(1920, 1080, image_src)
+
+    current = image_src
+    while True:
+        time.sleep(1)
+        results = sp.currently_playing()
+        image_src = results["item"]["album"]["images"][0]["url"]
+        if current != image_src:
+            current = image_src
+            draw_image(1920, 1080, image_src)
+        else:
+            print "same song"
+ 
     print image_src
 else:
     print "Can't get token for", username
